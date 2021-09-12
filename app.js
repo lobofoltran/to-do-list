c05a0e6db7fe24e2b558c602b9d2821fcb17b95f
Gustavo Lobo <gustavolobo@alunos.unicesumar.edu.br>
2021-09-12 16:33:17 -0300

    att

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function validaTamanhoTexto() {
+/*function validaTamanhoTexto() {
     let descricao = documento.getElementById('descricaoTarefa')
     if (descricao.value.length < 3) {
         document.getElementById('erroDescricao').style.display = 'block'
@@ -7,9 +7,15 @@ function validaTamanhoTexto() {
         console.log(descricao.value.length);
         document.getElementById('erroDescricao').style.display = 'none'
     }
-}
+}*/
 
 const listaTarefas = [];
+const guardar = [];
+
+// const '' = { "descricao": descricao, "status": "aberto", "prioridade": prioridade };
+// window.localStorage.setItem('guardar', JSON.stringify(guardar));
+
+renderLista();
 
 function fabricaTarefas() {
     let descricao = document.getElementById('descricaoTarefa').value;
@@ -18,7 +24,7 @@ function fabricaTarefas() {
         descricao: descricao,
         status: "aberto",
         prioridade: prioridade
-    };
+    }
     listaTarefas.push(tarefa);
     renderLista();
 }
@@ -27,21 +33,45 @@ function renderLista() {
     let divLista = document.getElementById('divLista');
     let template = '';
     for (let i = 0; i < listaTarefas.length; i++) {
-        template += `<div id="listaTarefas $[i]" class="tarefa ${listaTarefas[i].prioridade}"><center>
-                <p class="descTarefa"> ${listaTarefas[i].descricao}  </p>
-                <button onclick="excluirTarefa()" type="button" class="btn btn-danger">Excluir</button>
-                <input type="checkbox" name="" id="checkboxTarefas $[i]" onclick="marcarTarefa()"></center>
-                </div>`;
+        let tarefaConcluida = 'checked';
+        let classeTarefaConcluida = 'tarefaConcluida';
+        if (listaTarefas[i].status == 'aberto') {
+            tarefaConcluida = '';
+            classeTarefaConcluida = '';
+        }
+        template += `<div id="tarefa ${listaTarefas[i].prioridade}" class="tarefa ${listaTarefas[i].prioridade}">
+            <p class="descTarefa ${classeTarefaConcluida}"> ${listaTarefas[i].descricao} </p>
+            <button onclick="excluirTarefa(${i})" type="button" class="btn btn-danger">Excluir</button>
+            <input type="checkbox" onclick="concluirTarefa(${i})" name="" id="" ${tarefaConcluida}>
+            </div>`;
     }
     divLista.innerHTML = template;
 }
 
-function excluirTarefa() {
-    var elemento = document.getElementById('listaTarefas $[i]');
-    elemento.parentNode.removeChild(elemento);
+function excluirTarefa(indiceTarefa) {
+    if (confirm("Deseja remover a tarefa?")) {
+        listaTarefas.splice(indiceTarefa, 1)
+        // window.localStorage.removeItem(indiceTarefa);
+        renderLista();
+    }
 }
 
-function marcarTarefa() {
-var TacharElemento = document.getElementById('listaTarefas $[i]');
-TacharElemento.parentNode.textContent(String)
+function concluirTarefa(indiceTarefa) {
+    if (listaTarefas[indiceTarefa].status == 'aberto') {
+        listaTarefas[indiceTarefa].status = 'fechado';
+    } else {
+        listaTarefas[indiceTarefa].status = 'aberto';
+    }
+    renderLista();
 }
+
+function armazenarDados(indiceTarefa) {
+    var guardarDados = { "descricao": listaTarefas[indiceTarefa].descricao, "status": listaTarefas[indiceTarefa].status, "prioridade": listaTarefas[indiceTarefa].prioridade };
+    window.localStorage.setItem('guardarDados', JSON.stringify(guardarDados));
+}
+
+armazenarDados();
+
+// var guardarDados = { "descricao": listaTarefas[indiceTarefa].descricao, "status": listaTarefas[indiceTarefa].status, "prioridade": listaTarefas[indiceTarefa].prioridade };
+// var guardarDados = { "descricao": descricao, "status": "aberto", "prioridade": prioridade };
+// window.localStorage.setItem('guardar', JSON.stringify(guardar));
